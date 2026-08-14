c19fcf1d9bf4de4585b41e30357e05dc3c31caf9
ECHOES <mananaziz7887@gmail.com>
2026-08-14 15:09:30 +0500

    isssue solved

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  return {
-    plugins: [react(), tailwindcss()],
+  return {    base: '/eches.pk/',    plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
